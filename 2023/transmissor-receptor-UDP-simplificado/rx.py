0e1f0f2da011e32c329f4e86b6e3816562d90935
Faracoeng <alfmazucheli@gmail.com>
2023-02-25 19:53:08 -0300

    Comunicacao basica udp ajustando

--- 2023/transmissor-receptor-UDP-simplificado/rx.py
+++ 2023/transmissor-receptor-UDP-simplificado/rx.py
@@ -1,11 +1,20 @@
 import socket
 
 UDP_IP = "127.0.0.1"
-UDP_PORT = 5555
+UDP_PORT = 5005
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.bind((UDP_IP, UDP_PORT))
 
-while True:
-    data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
-    print("received message:", data)
+sock.settimeout(15)  # tempo limite de 5 segundos
+
+try:
+    while True:
+        data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
+        print("Mensagem recebida:", data.decode())
+        
+        # Envia uma resposta ao transmissor
+        MESSAGE = input("Digite uma mensagem: ")
+        sock.sendto(MESSAGE.encode(), addr)
+except socket.timeout:
+    print("Tempo limite de recepção ou transmissão excedido!")
--- 2023/transmissor-receptor-UDP-simplificado/tx.py
+++ 2023/transmissor-receptor-UDP-simplificado/tx.py
@@ -1,12 +1,19 @@
 import socket
 
+MESSAGE = input("Digite uma mensagem: ")
 UDP_IP = "127.0.0.1"
-UDP_PORT = 5555
-MESSAGE = b"Hello, World!"
-
-print("UDP target IP:", UDP_IP)
-print("UDP target port:", UDP_PORT)
-print("message:", MESSAGE)
+UDP_PORT = 5005
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-sock.sendto(MESSAGE, (UDP_IP, UDP_PORT))
+sock.settimeout(15)  # tempo limite de 5 segundos
+
+try:
+    # Envia a mensagem ao receptor
+    sock.sendto(MESSAGE.encode(), (UDP_IP, UDP_PORT))
+    
+    # Espera por uma resposta do receptor
+    data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
+    print("Resposta do receptor:", data.decode())
+    
+except socket.timeout:
+    print("Tempo limite de transmissão excedido!")
