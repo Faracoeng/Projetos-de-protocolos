1b9fa1660bf834484dc53d866754aadedc3dc789
Faracoeng <alfmazucheli@gmail.com>
2023-03-10 14:33:33 -0300

    Recendo mensagens

--- 2023/Mensagens/msg.py
+++ 2023/Mensagens/msg.py
@@ -13,6 +13,22 @@ class Data(Mensagem):
         if len(body)> 512:
             raise ValueError('Corpo deve ter no máximo 512 Bytes')
         self.body = body
+
+    def serialize(self):
+        
+        msg = Mensagem.serialize(self)
+        extra = struct.pack('!H', self._blocknum)
+        return msg + extra + self.body
+
 def cria_instancia(msg:bytes):
     print()
     # Criar uma instancia de subclasse da classe "Mensagem"
+
+
+# Para decodificar as mensagens que chegam:
+
+# Aqui descompacta os 4 prmeiros bytes
+struct.unpack_from('!HH',msg)
+# Aqui pega o restante da mensagem que não qual o tamanhho,
+# por isso fazer assim
+body = msg[struct.calcsize('!HH'):]
